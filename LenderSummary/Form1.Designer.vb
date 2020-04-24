250551bf43eaf477cc4b7fb698149b0db9cde813
Ian Stabler <Ian@investandfund.com>
2020-04-24 17:09:01 +0100

    Add project files.

---
+++ LenderSummary/Form1.Designer.vb
@@ -0,0 +1,176 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.frmAccountName = New System.Windows.Forms.Label()
+        Me.frmAccountID = New System.Windows.Forms.TextBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Go = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.frmLoanID = New System.Windows.Forms.TextBox()
+        Me.frmLHID = New System.Windows.Forms.TextBox()
+        Me.frmLoanName = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.frmLoanExtr = New System.Windows.Forms.TextBox()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(63, 11)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(47, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Account"
+        '
+        'frmAccountName
+        '
+        Me.frmAccountName.AutoSize = True
+        Me.frmAccountName.Location = New System.Drawing.Point(231, 9)
+        Me.frmAccountName.Name = "frmAccountName"
+        Me.frmAccountName.Size = New System.Drawing.Size(39, 13)
+        Me.frmAccountName.TabIndex = 1
+        Me.frmAccountName.Text = "Label2"
+        '
+        'frmAccountID
+        '
+        Me.frmAccountID.Location = New System.Drawing.Point(115, 6)
+        Me.frmAccountID.Name = "frmAccountID"
+        Me.frmAccountID.Size = New System.Drawing.Size(100, 20)
+        Me.frmAccountID.TabIndex = 2
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(8, 113)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(1306, 500)
+        Me.DataGridView1.TabIndex = 3
+        '
+        'Go
+        '
+        Me.Go.Location = New System.Drawing.Point(1239, 5)
+        Me.Go.Name = "Go"
+        Me.Go.Size = New System.Drawing.Size(75, 68)
+        Me.Go.TabIndex = 4
+        Me.Go.Text = "Go"
+        Me.Go.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(78, 33)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(31, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Loan"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(40, 57)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(70, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Loan Holding"
+        '
+        'frmLoanID
+        '
+        Me.frmLoanID.Location = New System.Drawing.Point(115, 30)
+        Me.frmLoanID.Name = "frmLoanID"
+        Me.frmLoanID.Size = New System.Drawing.Size(100, 20)
+        Me.frmLoanID.TabIndex = 7
+        '
+        'frmLHID
+        '
+        Me.frmLHID.Location = New System.Drawing.Point(115, 54)
+        Me.frmLHID.Name = "frmLHID"
+        Me.frmLHID.Size = New System.Drawing.Size(100, 20)
+        Me.frmLHID.TabIndex = 8
+        '
+        'frmLoanName
+        '
+        Me.frmLoanName.AutoSize = True
+        Me.frmLoanName.Location = New System.Drawing.Point(231, 33)
+        Me.frmLoanName.Name = "frmLoanName"
+        Me.frmLoanName.Size = New System.Drawing.Size(39, 13)
+        Me.frmLoanName.TabIndex = 9
+        Me.frmLoanName.Text = "Label4"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(14, 81)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(95, 13)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "part of Loan Name"
+        '
+        'frmLoanExtr
+        '
+        Me.frmLoanExtr.Location = New System.Drawing.Point(115, 78)
+        Me.frmLoanExtr.Name = "frmLoanExtr"
+        Me.frmLoanExtr.Size = New System.Drawing.Size(239, 20)
+        Me.frmLoanExtr.TabIndex = 11
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1321, 621)
+        Me.Controls.Add(Me.frmLoanExtr)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.frmLoanName)
+        Me.Controls.Add(Me.frmLHID)
+        Me.Controls.Add(Me.frmLoanID)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Go)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.frmAccountID)
+        Me.Controls.Add(Me.frmAccountName)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Lender Summary"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents frmAccountName As Label
+    Friend WithEvents frmAccountID As TextBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Go As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents frmLoanID As TextBox
+    Friend WithEvents frmLHID As TextBox
+    Friend WithEvents frmLoanName As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents frmLoanExtr As TextBox
+End Class
---
+++ LenderSummary/Form1.vb
@@ -0,0 +1,438 @@
+﻿Imports System.Configuration
+Imports System.Collections.Specialized
+Imports System.IO
+Imports System.Data
+Imports System.Data.SqlClient
+
+Public Class Form1
+
+    Public Class Extract
+        Property AccountID As String
+        Property LoanID As String
+        Property LoanName As String
+        Property LHID As String
+        Property OrderID As String
+        Property TransactionDate As String
+        Property TransType As Integer
+        Property TransDesc As String
+        Property Amount As Integer
+        Property LHAmtIn As String
+        Property LHAmtOut As String
+        Property LHBal As Integer
+        Property PIBal As Integer
+
+
+    End Class
+
+    Public dsLenders As DataSet
+    Public dsLoans As DataSet
+    Public dsOrders As DataSet
+    Public dsTrans As DataSet
+    Public dsHoldings As DataSet
+    Public ExtractName As String
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        frmAccountName.Text = ""
+        frmLoanName.Text = ""
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Go.Click
+        Dim MySQL, strConn, sHTML, bHTML, sUsers As String
+        Dim MyConn As FirebirdSql.Data.FirebirdClient.FbConnection
+        Dim Cmd As FirebirdSql.Data.FirebirdClient.FbCommand
+        Dim Adaptor As FirebirdSql.Data.FirebirdClient.FbDataAdapter
+        Dim dr1, dr2, dr3 As DataRow
+        Dim environ As String = "L"
+        Dim connection As String = "FBConnectionString"
+        Dim iAccountid, iLoanID, iLHID, tLoanID As Integer
+        Dim iParentLid As Integer = Nothing
+        Dim xBusinessName As String = Nothing
+        Dim xParentBusName As String = Nothing
+        Dim ExtractList As New List(Of Extract)
+        Dim PrevAccountid As Integer = Nothing
+        Dim dispLoanID, dispLoanName, dispLHID, disporderid, dispDate, dispAccountID As String
+        Dim printedLoanid, printedOrderID, printedLHID, printedAccountID As Integer
+        Dim SomethingSupplied As Integer = Nothing
+        Dim sqlAccount As String = ""
+        Dim sqlAccount2 As String = ""
+        Dim sqlAccount3 As String = ""
+        Dim sqlLoan As String = ""
+        Dim sqlLHID As String = ""
+
+
+        sqlAccount = ""
+        sqlAccount2 = ""
+        sqlAccount3 = ""
+        If IsNumeric(frmAccountID.Text) Then
+            iAccountid = frmAccountID.Text
+            strConn = ConfigurationManager.ConnectionStrings(connection).ConnectionString
+            MyConn = New FirebirdSql.Data.FirebirdClient.FbConnection(strConn)
+            MyConn.Open()
+
+            MySQL = "select a.accountid, trim(u.firstname) as firstname, trim (u.lastname) as lastname, trim (u.companyname) as companyname
+                  from accounts a , users u
+                  where a.accountid = " & iAccountid &
+                  " and a.userid = u.userid "
+
+            dsLenders = New DataSet
+            Adaptor = New FirebirdSql.Data.FirebirdClient.FbDataAdapter(MySQL, MyConn)
+
+            Adaptor.Fill(dsLenders)
+
+            MyConn.Close()
+
+            Dim iLenderCounter As Integer = dsLenders.Tables(0).Rows.Count
+
+            For i = 0 To iLenderCounter - 1
+                dr1 = dsLenders.Tables(0).Rows(i)
+
+                xBusinessName = dr1("firstname") & " " & dr1("lastname") & " - " & dr1("companyname")
+
+                frmAccountName.Text = xBusinessName
+                sqlAccount = " and o.accountid =  " & iAccountid
+                sqlAccount2 = " and f.accountid =  " & iAccountid
+                sqlAccount3 = " and b.accountid =  " & iAccountid
+                SomethingSupplied = 1
+            Next
+        End If
+
+        If frmLoanExtr.Text <> "" And IsNumeric(frmLoanID.Text) Then
+            frmLoanExtr.Text = ""
+        End If
+
+
+        sqlLoan = ""
+        If frmLoanExtr.Text <> "" Then
+            Dim xLoanExtr As String = "'%" & frmLoanExtr.Text & "%'"
+            xLoanExtr = xLoanExtr.ToLower
+            strConn = ConfigurationManager.ConnectionStrings(connection).ConnectionString
+            MyConn = New FirebirdSql.Data.FirebirdClient.FbConnection(strConn)
+            MyConn.Open()
+
+            MySQL = "select l.loanid, trim(l.business_name) as business_name
+                  from loans l
+                  where lower(l.business_name) like " & xLoanExtr
+
+            dsLoans = New DataSet
+            Adaptor = New FirebirdSql.Data.FirebirdClient.FbDataAdapter(MySQL, MyConn)
+
+            Adaptor.Fill(dsLoans)
+
+            MyConn.Close()
+
+            Dim iLoanCounter As Integer = dsLoans.Tables(0).Rows.Count
+
+            If iLoanCounter > 0 Then
+                sqlLoan = " and h.loanid in ( "
+            End If
+            For i = 0 To iLoanCounter - 1
+                dr1 = dsLoans.Tables(0).Rows(i)
+
+                tLoanID = dr1("loanid")
+
+
+                sqlLoan = sqlLoan & tLoanID & ", "
+                SomethingSupplied = 1
+            Next
+            If iLoanCounter > 0 Then
+                sqlLoan = sqlLoan & " 0) "  'there are no 0 loans so this will not pick up anything extra
+            End If
+
+        End If
+
+        If IsNumeric(frmLoanID.Text) Then
+            iLoanID = frmLoanID.Text
+            strConn = ConfigurationManager.ConnectionStrings(connection).ConnectionString
+            MyConn = New FirebirdSql.Data.FirebirdClient.FbConnection(strConn)
+            MyConn.Open()
+
+            MySQL = "select l.loanid, trim(l.business_name) as business_name
+                  from loans l
+                  where l.loanid = " & iLoanID
+
+
+            dsLoans = New DataSet
+            Adaptor = New FirebirdSql.Data.FirebirdClient.FbDataAdapter(MySQL, MyConn)
+
+            Adaptor.Fill(dsLoans)
+
+            MyConn.Close()
+
+            Dim iLoanCounter As Integer = dsLoans.Tables(0).Rows.Count
+
+            For i = 0 To iLoanCounter - 1
+                dr1 = dsLoans.Tables(0).Rows(i)
+
+                xBusinessName = dr1("business_name")
+
+                frmLoanName.Text = xBusinessName
+                sqlLoan = " and h.loanid =  " & iLoanID
+                SomethingSupplied = 1
+            Next
+        End If
+
+        sqlLHID = ""
+        If IsNumeric(frmLHID.Text) Then
+            strConn = ConfigurationManager.ConnectionStrings(connection).ConnectionString
+            MyConn = New FirebirdSql.Data.FirebirdClient.FbConnection(strConn)
+            iLHID = frmLHID.Text
+
+            sqlLHID = " and h.loan_holdings_id =  " & iLHID
+            SomethingSupplied = 1
+
+        End If
+
+        If SomethingSupplied = 1 Then
+            MyConn.Open()
+
+            'MySQL = "select distinct o.orderid, o.lh_id, h.loanid, trim(l.business_name) as business_name 
+            '        from orders o, loan_holdings h, loans l
+            '        where o.accountid = " & iAccountid &
+            '        " and o.lh_id = h.loan_holdings_id
+            '        and h.loanid = l.loanid
+            '         order by h.loanid, h.loan_holdings_id, o.orderid "
+
+            MySQL = "Select orderid, lh_id, loanid, business_name, accountid
+                    from
+                    (select distinct o.orderid as orderid, o.lh_id as lh_id, h.loanid as loanid, trim(l.business_name) as business_name, o.accountid as accountid
+                    from orders o, loan_holdings h, loans l, accounts a, users u
+                    where
+                    o.lh_id = h.loan_holdings_id
+                    and o.accountid = a.accountid
+                    and u.userid = a.userid
+                    and u.usertype = 0
+                    and h.loanid = l.loanid
+                    " & sqlAccount & sqlLoan & sqlLHID & "
+                      union all
+                    select distinct o.orderid as orderid, h.loan_holdings_id as lh_id, h.loanid as loanid, trim(l.business_name) as business_name, o.accountid as accountid
+                    from orders o, loan_holdings h, loans l, lh_balances b, bids d, accounts a, users u
+                    where 
+                    o.lh_id = 0
+                    and h.loanid = l.loanid
+                    and o.loanid = h.loanid
+                    and b.lh_id = h.loan_holdings_id
+                    and b.accountid = o.accountid
+                    and h.bidid = d.bidid
+                    and o.accountid = a.accountid
+                    and u.userid = a.userid
+                    and u.usertype = 0
+                    and o.orderid = d.orderid
+                    " & sqlAccount3 & sqlLoan & sqlLHID & "
+                    )   results
+                    order by loanid, lh_id, accountid, orderid"
+
+            dsOrders = New DataSet
+            Adaptor = New FirebirdSql.Data.FirebirdClient.FbDataAdapter(MySQL, MyConn)
+
+            Adaptor.Fill(dsOrders)
+
+            MyConn.Close()
+            Dim iOrderCounter As Integer = dsOrders.Tables(0).Rows.Count
+            Dim prevLoanId, prevOrderid, prevLHID, thisLoanId, thisOrderid, thisLHID, thisAccountid As Integer
+            Dim loanname As String
+            Dim prevDate, thisDate As Date
+
+
+
+            Dim CurrLHBal As Integer
+            Dim PIBal As Integer
+
+            For i = 0 To iOrderCounter - 1
+                dr2 = dsOrders.Tables(0).Rows(i)
+                thisLoanId = dr2("loanid")
+                thisOrderid = dr2("orderid")
+                thisLHID = dr2("lh_id")
+                thisAccountID = dr2("Accountid")
+
+
+
+                If thisLoanId <> printedLoanid Then
+                    prevLoanId = thisLoanId
+                    CurrLHBal = 0
+                    PIBal = 0
+                    loanname = dr2("business_name")
+                    dispLoanID = thisLoanId
+                    dispLoanName = loanname
+                    dispLHID = prevLHID
+                    dispAccountID = prevAccountid
+
+                Else
+                    dispLoanID = Nothing
+                    dispLoanName = Nothing
+                    dispLHID = Nothing
+                    dispAccountID = prevAccountid
+                End If
+
+                If thisLHID <> printedLHID Then
+                    prevLHID = thisLHID
+                    CurrLHBal = 0
+                    PIBal = 0
+                    dispLHID = prevLHID
+                Else
+                    dispLHID = Nothing
+                End If
+
+                If thisAccountid <> printedAccountID Then
+                    prevAccountid = thisAccountid
+                    CurrLHBal = 0
+                    PIBal = 0
+                    dispAccountID = prevAccountid
+                Else
+                    dispAccountID = Nothing
+                End If
+
+
+                If thisOrderid <> printedOrderID Then
+                    prevOrderid = thisOrderid
+                    disporderid = thisOrderid
+                    MySQL = "Select f.datecreated, f.amount,  f.transtype,  f.isactive
+                            From fin_trans f
+                            Where  
+                            f.orderid = " & thisOrderid &
+                            " and f.isactive = 0
+                            and f.accountid = " & thisAccountid &
+                            " and f.transtype not in (1200, 1201, 1203, 1204, 1300, 1302)  
+                            order by f.datecreated, f.transtype "
+
+                    dsTrans = New DataSet
+                    Adaptor = New FirebirdSql.Data.FirebirdClient.FbDataAdapter(MySQL, MyConn)
+
+                    Adaptor.Fill(dsTrans)
+
+                    MyConn.Close()
+                    Dim iTransCounter As Integer = dsTrans.Tables(0).Rows.Count
+                    For j = 0 To iTransCounter - 1
+                        Dim newExtract As New Extract
+
+                        dr3 = dsTrans.Tables(0).Rows(j)
+                        newExtract.AccountID = dispAccountID
+                        newExtract.LoanID = dispLoanID
+                        newExtract.LoanName = dispLoanName
+                        newExtract.LHID = dispLHID
+                        newExtract.OrderID = disporderid
+
+                        thisDate = dr3("datecreated")
+                        If thisDate = prevDate Then
+                            dispDate = Nothing
+                        Else
+                            dispDate = dr3("datecreated")
+                        End If
+                        newExtract.TransactionDate = dispDate
+                        prevDate = thisDate
+                        newExtract.TransType = dr3("transtype")
+                        Dim itranstype As Integer = dr3("transtype")
+                        Dim xtransdesc As String = Nothing
+                        Dim xIO As String = Nothing
+                        Transtype(itranstype, xtransdesc, xIO)
+                        newExtract.TransDesc = xtransdesc
+                        If xIO = "I" Then
+                            newExtract.LHAmtIn = dr3("amount")
+                            newExtract.LHAmtOut = ""
+                            CurrLHBal += dr3("amount")
+                        End If
+                        If xIO = "O" Then
+                            newExtract.LHAmtIn = ""
+                            newExtract.LHAmtOut = dr3("amount")
+                            CurrLHBal -= dr3("amount")
+                        End If
+                        If itranstype = (1408 Or 1412) Then
+                            PIBal += dr3("amount")
+                        End If
+                        If itranstype = (1409 Or 1410) Then
+                            PIBal -= dr3("amount")
+                        End If
+                        newExtract.Amount = dr3("amount")
+                        newExtract.LHBal = CurrLHBal
+                        newExtract.PIBal = PIBal
+                        ExtractList.Add(newExtract)
+                        disporderid = Nothing
+                        dispLoanID = Nothing
+                        dispLHID = Nothing
+                        dispLoanName = Nothing
+                        dispAccountid = Nothing
+                        printedLoanid = thisLoanId
+                        printedOrderID = thisOrderid
+                        printedLHID = thisLHID
+                        printedAccountID = thisAccountid
+
+                    Next
+                End If
+
+
+
+            Next
+
+            DataGridView1.DataSource = ExtractList
+            DataGridView1.Columns(0).Width = 50
+            DataGridView1.Columns(1).Width = 50
+            DataGridView1.Columns(2).Width = 240
+            DataGridView1.Columns(3).Width = 50
+            DataGridView1.Columns(4).Width = 50
+            DataGridView1.Columns(5).Width = 120
+            DataGridView1.Columns(6).Width = 60
+            DataGridView1.Columns(7).Width = 240
+            DataGridView1.Columns(8).Width = 80
+            DataGridView1.Columns(9).Width = 80
+            DataGridView1.Columns(10).Width = 80
+            DataGridView1.Columns(11).Width = 80
+            DataGridView1.Columns(12).Width = 80
+
+        End If
+    End Sub
+
+    Public Function Transtype(ByVal iTranstype As Integer, ByRef xtransdesc As String, ByRef xIO As String)
+        Select Case iTranstype
+            Case 1206
+                xtransdesc = "Auction Completion"
+                xIO = "I"
+            Case 1208
+                xtransdesc = "Auction Drawdown"
+                xIO = "I"
+            Case 1301
+                xtransdesc = "Capital on Loan Repayment"
+                xIO = "O"
+            Case 1303
+                xtransdesc = "Earned Interest Received"
+                xIO = "x"
+            Case 1304
+                xtransdesc = "Facility Fee Paid"
+                xIO = "x"
+            Case 1400
+                xtransdesc = "Loan Part Sale"
+                xIO = "O"
+            Case 1401
+                xtransdesc = "Loan Part Purchase"
+                xIO = "I"
+            Case 1402
+                xtransdesc = "Transaction Fees Paid"
+                xIO = "x"
+            Case 1406
+                xtransdesc = "Transaction Fee Paid"
+                xIO = "x"
+            Case 1408
+                xtransdesc = "Purchased Interest Bought"
+                xIO = "x"
+            Case 1409
+                xtransdesc = "Repayment of Purchased Interest"
+                xIO = "x"
+            Case 1410
+                xtransdesc = "Purchased Interest on Repayment"
+                xIO = "x"
+            Case 1411
+                xtransdesc = "Earned Interest on Repayment"
+                xIO = "x"
+            Case 1412
+                xtransdesc = "Purchase of Earned Interest"
+                xIO = "x"
+            Case 1413
+                xtransdesc = "Earned Interest Received"
+                xIO = "x"
+
+            Case Else
+                xtransdesc = ""
+                xIO = ""
+        End Select
+
+    End Function
+
+End Class
